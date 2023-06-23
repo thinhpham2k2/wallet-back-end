97fa7c763d2622c1402ca4fa3e278c32aa126dc8
thinhpham2k2 <thinhphamquoc9999@gmail.com>
2023-06-23 13:16:26 +0700

    Update API

--- src/main/java/com/wallet/config/SecurityConfig.java
+++ src/main/java/com/wallet/config/SecurityConfig.java
@@ -57,7 +57,8 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable();
+        http.cors().disable();
+        http.csrf().disable();
         http.authorizeHttpRequests().requestMatchers("/partner/api/programs/token", "/swagger-ui/**", "/v3/api-docs/**","/api/auth/google/**", "/partner/api/partners/register", "/api/auth/login").permitAll()
                 .anyRequest().authenticated();
 
