fd460c225c84251f791316942ffac9662a08870e
thinhpham2k2 <thinhphamquoc9999@gmail.com>
2023-06-23 14:41:01 +0700

    Update API

--- src/main/java/com/wallet/config/CORSConfig.java
+++ src/main/java/com/wallet/config/CORSConfig.java
@@ -11,7 +11,7 @@ public class CORSConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedMethods("*")
-                .allowedOrigins("http://localhost:3000", "https://swd392-pointify-partner.vercel.app")
+                .allowedOrigins("http://localhost:3000", "https://*")
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(-1);
